dde05cc58d32d1c51bb32051d92479e24bdbf6c3
andrewmumblebee <aceharris@hotmail.co.uk>
2023-05-22 11:33:03 +0100

    fix: bio sounded weird

--- components/About.tsx
+++ components/About.tsx
@@ -6,8 +6,9 @@ export default function About() {
         I&apos;m a developer based in London. With over 5 years of experience,
         over my career I&apos;ve had a chance to work on a spectrum of web
         development.
-        <br />I love working on frontend experiences, but I&apos;m also
-        comfortable in the backend.
+        <br />I love working on frontend experiences, but enjoy working across
+        the full stack. I have a passion for learning new things and keeping up
+        to date with the latest technologies.
       </p>
     </section>
   )
